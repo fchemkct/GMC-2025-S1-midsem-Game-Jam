aabc76c98c64470fc40a3e546519b00ef1509dbf
fchemkct <hieu24a4@gmail.com>
2025-04-19 04:07:23 +1000

    basic player movement, animation and collision dectection with npc
    player: moves 4 direction, change animations, detect collision
    
    npc: has collision

---
+++ gmc-game-jam-semester-1-mid-sem/player.gd
@@ -0,0 +1,47 @@
+extends Area2D
+
+signal inRange
+
+@export var speed = 400
+var screenSize
+var canTalk = false; ##determines if player is in range to talk to another npc
+
+func _ready():
+	screenSize = get_viewport_rect().size
+	
+func _process(delta: float):
+	walk(delta)
+	pass
+	
+func walk(delta: float):
+	var velocity = Vector2.ZERO # The player's movement vector.
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
+
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite2D.play()
+	else:
+		$AnimatedSprite2D.stop()
+		
+	position += velocity * delta
+	position = position.clamp(Vector2.ZERO, screenSize)
+
+
+func _on_body_entered(body) -> void:
+	##hide()
+	##inRange.emit()
+	canTalk = true
+	print("enter:cantalk bool is: " + str(canTalk))
+	 # Replace with function body.
+
+func _on_body_exited(body) -> void:
+	canTalk = false
+	print("exit:cantalk bool is: " + str(canTalk))
+	 # Replace with function body.
