1c6a398ddd9658eea3e7162aff0858a4fe566052
fchemkct <hieu24a4@gmail.com>
2025-04-21 21:57:30 +1000

    tauri scripts implemented

--- gmc-game-jam-semester-1-mid-sem/Story storage files/tauri/tauri_dialogue_file.gd
+++ gmc-game-jam-semester-1-mid-sem/Story storage files/tauri/tauri_dialogue_file.gd
@@ -1,12 +1,12 @@
 extends Control
 
 @export var lines: Array[String] = [
-	"You watch as Tauri and Orion discuss matters of the corpse",
-	"The basic deduction is that Lacaille died sometime last night, with no injuries other than a blunt head wound speaker:Tauri",
-	"No signs of physical struggle on her attire, nor oddities found around the road where she was laying.",
-	"(Why does the town doctor seem so familiar with autopsies?) You wonder.",
-	"Orion seems to hear your question, and whispers to you as he flips through the report in his hands.",
-	"I can ask her if you’d like. But such a personal question may require a little nudge.",
-	"…Try tracing the flow of my magic; draw the pattern, if you will.",
-	"#spellcasts",
+	"You watch as Tauri and Orion discuss matters of the corpse. #speaker:Narrator",
+"The basic deduction is that Lacaille died sometime last night, with no injuries other than a blunt head wound. #speaker:Tauri",
+"No signs of physical struggle on her attire, nor oddities found around the road where she was laying.",
+"(Why does the town doctor seem so familiar with autopsies?) You wonder. #speaker:Narrator",
+"Orion seems to hear your question, and whispers to you as he flips through the report in his hands.",
+"I can ask her if you’d like. But such a personal question may require a little nudge. #speaker:Orion",
+"…Try tracing the flow of my magic; draw the pattern, if you will.",
+"#spellcasts",
 ]
