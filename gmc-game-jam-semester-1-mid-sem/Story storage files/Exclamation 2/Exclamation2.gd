bda2170f2f635df6407bd8d59e399a7679396c79
fchemkct <hieu24a4@gmail.com>
2025-04-22 03:25:46 +1000

    second exclamation guy

---
+++ gmc-game-jam-semester-1-mid-sem/Story storage files/Exclamation 2/Exclamation2.gd
@@ -0,0 +1,10 @@
+extends Control
+
+@export var lines: Array[String] = [
+"(Are we sure that Gien’s testimony was accurate?) #speaker:Narrator",
+"You may not know this, but Gien is known for his strong memory and facial recognition. #speaker:Orion",
+"He’s worked in many towns before, including large ones that are comparable to The City. #speaker:Orion", 
+"And as far as our spell’s influence goes… it’s unlikely he lied after it was cast. #speaker:Orion",
+"#choices",
+
+]
---
+++ gmc-game-jam-semester-1-mid-sem/Story storage files/Exclamation 2/Exclamation2_after_choice.gd
@@ -0,0 +1,10 @@
+extends Control
+
+@export var lines: Array[String] = [
+"Yes. Sometimes cases simply turn out to be unfortunate accidents, rather than murders. #speaker:Orion",
+"But I won’t rule out the possibility of foul play until we have more clues. If only for the sake of the deceased.",
+"What we know now is that the cause of death was a head wound, and blood was found on the ground, meaning it was likely a fall and impact.",
+"There seems to be no other injuries or indications of physical force.",
+"And rather than hiding the body… the culprit may have only cared about erasing their tracks.",
+"Let’s speak to Sadal. She runs the tavern; since the death occurred at late hours, maybe she remembers who’d been out last night.",
+]
