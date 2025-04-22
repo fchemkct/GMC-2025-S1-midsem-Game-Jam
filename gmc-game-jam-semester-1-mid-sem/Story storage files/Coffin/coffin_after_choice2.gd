a23e74af20d30ba0b09040fc3564c082f170964e
fchemkct <hieu24a4@gmail.com>
2025-04-22 20:43:17 +1000

    fix: coffin

--- gmc-game-jam-semester-1-mid-sem/Story storage files/Coffin/coffin_after_choice2.gd
+++ gmc-game-jam-semester-1-mid-sem/Story storage files/Coffin/coffin_after_choice2.gd
@@ -1,9 +1,8 @@
 extends Control
 
 @export var lines: Array[String] = [
-"A small smile tugs on Orion’s lips. #speaker:Narrator",
-"Interesting… I’ve never run into something like this before. #speaker:Orion",
-"Since only I can hear and sense you, why don’t you come along on my investigation?",
-"It may be helpful to have a second opinion. Especially one that may provide a new perspective on things.",
-"#choice2",
+"Let’s visit Tauri first. She’s the town doctor and would’ve made a report on the body, since she was the one to help Sheriff Cor transfer it into the coffin. #speaker:Orion",
+"I was in a neighboring town for work this morning… so I wasn’t here to witness how Lacaille had been found.",
+"But I’m sure Tauri’s notes will be just as detailed as her regular demeanor. Her clinic is just up north.",
+"end:Tauri"
 ]
