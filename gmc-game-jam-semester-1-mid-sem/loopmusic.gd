a73e9f497c10799a4fe93b05fb2a961994d9c5aa
ee amanda <eeamanda2000@gmail.com>
2025-04-23 01:43:17 +1000

    music added

--- gmc-game-jam-semester-1-mid-sem/loopmusic.gd
+++ gmc-game-jam-semester-1-mid-sem/loopmusic.gd
@@ -1,7 +1,7 @@
 extends Node
 
 func _ready():
-	var audio = load("res://audio/bgm.ogg") as AudioStream
+	var audio = load("res://31 BIDØ - Infinite 24 Bit MASTER.mp3") as AudioStream
 	audio.loop = true  # Set loop flag in code
 	$AudioStreamPlayer.stream = audio
 	$AudioStreamPlayer.play()
