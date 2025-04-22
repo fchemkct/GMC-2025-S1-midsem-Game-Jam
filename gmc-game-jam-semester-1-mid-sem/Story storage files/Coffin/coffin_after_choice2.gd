6de859952db223b5d088385e23434e5ed835416f
fchemkct <hieu24a4@gmail.com>
2025-04-22 20:38:34 +1000

    fix coffin

---
+++ gmc-game-jam-semester-1-mid-sem/Story storage files/Coffin/coffin_after_choice2.gd
@@ -0,0 +1,9 @@
+extends Control
+
+@export var lines: Array[String] = [
+"A small smile tugs on Orion’s lips. #speaker:Narrator",
+"Interesting… I’ve never run into something like this before. #speaker:Orion",
+"Since only I can hear and sense you, why don’t you come along on my investigation?",
+"It may be helpful to have a second opinion. Especially one that may provide a new perspective on things.",
+"#choice2",
+]
