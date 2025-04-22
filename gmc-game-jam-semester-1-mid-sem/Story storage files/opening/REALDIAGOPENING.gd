fc32ef83a90294f584a2dfbf8b952426c1b65ac5
fchemkct <hieu24a4@gmail.com>
2025-04-23 00:50:16 +1000

    push newest

--- gmc-game-jam-semester-1-mid-sem/Story storage files/opening/REALDIAGOPENING.gd
+++ gmc-game-jam-semester-1-mid-sem/Story storage files/opening/REALDIAGOPENING.gd
@@ -69,7 +69,7 @@ func display_next_line():
 		##get_tree().change_scene_to_file("res://spellcast_1.tscn")
 
 func _input(event):
-	if event.is_action_pressed("ui_accept") && !choice_on && !spell_on:  # Press Enter/Space/etc
+	if event.is_action_pressed("dialogue") && !choice_on && !spell_on:  # Press Enter/Space/etc
 		display_next_line()
 		
 func _dialogueEnd():
