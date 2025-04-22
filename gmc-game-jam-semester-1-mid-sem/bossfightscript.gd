cfda4dcff64cc6567b51dcdfa75a18f293893194
ee amanda <eeamanda2000@gmail.com>
2025-04-22 22:54:28 +1000

    adding scripts and nodes

--- gmc-game-jam-semester-1-mid-sem/bossfightscript.gd
+++ gmc-game-jam-semester-1-mid-sem/bossfightscript.gd
@@ -4,5 +4,11 @@ extends Node
 "What doesn’t? #speaker:Orion",
 "Convicting Arneb without full proof. Horo’s ramblings about a sacrifice. ‘Everything.’ #speaker:Narrator",
 "You think about Arneb’s lost and distraught expression. And suddenly, a mental image of the same 
-girl smiling, bubbly and bright — flashes through your mind."
+girl smiling, bubbly and bright — flashes through your mind.",
+"I think… We need more clues. #speaker:Narrator",
+"‘I mean, what about Lacaille herself? Maybe looking into her room, or home, will tell us more—’ #speaker:Narrator",
+"Sometimes, the only way to combat a mass murderer is to use unusual methods. #speaker:Orion",
+"Even if it may put someone else in a complicated place.", 
+"Someone like Arneb doesn’t seem capable of such a crime, though. #speaker:Narrator",
+"You aren’t sure when you gained such a strong belief in her character, but something in your gut tells you this is true."
 ]
---
+++ gmc-game-jam-semester-1-mid-sem/exclamation5.gd
@@ -0,0 +1,6 @@
+extends Node
+
+@export var lines: Array[String] = [
+"Finally, you have a chance to ask Orion some questions. #speaker:Narrator",
+"#choices"
+]
